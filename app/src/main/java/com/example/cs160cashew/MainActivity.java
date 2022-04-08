8e5f4c9998a56b10e520aefcd5f8ad5aa461ab04
Min-yuan Lee <min-yuan.lee@sjsu.edu>
2022-04-07 22:37:59 -0700

    Add splash screen and cashew men logo

--- app/src/main/java/com/example/cs160cashew/MainActivity.java
+++ app/src/main/java/com/example/cs160cashew/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         Button login = (Button) findViewById(R.id.loginbutton);
         login.setOnClickListener(new View.OnClickListener() {
