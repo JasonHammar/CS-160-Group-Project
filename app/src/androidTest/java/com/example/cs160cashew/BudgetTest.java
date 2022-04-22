44dd2709e4a4a9ee62218a99e1a5847c4766ffec
Aidan Kormanik <aidan.kormanik@sjsu.edu>
2022-04-21 20:35:57 -0700

    Adding the white box test files to branch

---
+++ app/src/androidTest/java/com/example/cs160cashew/BudgetTest.java
@@ -0,0 +1,44 @@
+package com.example.cs160cashew;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BudgetTest extends junit.framework.TestCase {
+    Budget budget;
+    String name = "Test name";
+    Category category = new Category("Test category");
+    Category extraCategory = new Category("Extra category");
+    List<Category> saved;
+    int limit = 240;
+
+    protected void setUp() {
+        budget = new Budget(name, category, limit);
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals(budget.getName(), name);
+    }
+
+    @Test
+    public void testGetLimit() {
+        assertEquals(budget.getLimit(), limit);
+    }
+
+    @Test
+    public void testGetCategory() {
+        assertFalse(budget.getCategoryList().isEmpty());
+    }
+
+    @Test
+    public void testAddCategory() throws Exception {
+        saved = budget.getCategoryList();
+        budget.addCategory(extraCategory);
+        assertNotEquals(budget.getCategoryList(), saved);
+    }
+
+}
